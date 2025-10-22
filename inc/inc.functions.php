dd2a54a498609a97d916d1a152c7561dd08c846e
ParisAlan <alanparis2003@gmail.com>
2025-10-22 14:56:11 +0200

    Rajout de commentaire sur chacune des fonctions de inc.functions.php

--- inc/inc.functions.php
+++ inc/inc.functions.php
@@ -1,13 +1,20 @@
 <?php
+    // On initialise la session avec "session_start" afin de pouvoir récupérer les informations présentes
+    // à l'intérieur
     session_start();
 
+    // On définit les différentes constantes et on leur donne une valeur par défaut.
     define('TL_ROOT', dirname(__DIR__));
     define('LOGIN', 'UEL311');
     define('PASSWORD', 'U31311');
     define('DB_ARTICLES', TL_ROOT.'/dbal/articles.json');
 
+    // Cette fonction a pour objectif de connecter l'utilisateur si il rentre les identifiants attendus
     function connectUser($login = null, $password = null){
+        // La première boucle if va vérifier que les informations mises par l'utilisateur ne sont pas nuls
         if(!is_null($login) && !is_null($password)){
+            // Dans le cadre ou les informations mises par l'utilisateurs correspondent à les informations attendus
+            // alors, on return
             if($login === LOGIN && $password === PASSWORD){
                 return array(
                     'login'    => LOGIN,
@@ -18,11 +25,14 @@
         return null;
     }
 
+    // On déconnecte l'utilisateur en détruisant la variable de session user, puis en détruisant la session avec session_destroy
     function setDisconnectUser(){
          unset($_SESSION['User']);
-         sessions_destroy();
+         session_destroy();
     }
 
+    // On vérifie que l'utilisateur est connecté avec le mot clé array_key_exist. La fonction vérifie que la valeur de session
+    // User existe, qu'elle n'est pas = null et qu'elle n'est pas vide. Si c'est le cas, return true, sinon, return false.
     function isConnected(){
         if(array_key_exists('User', $_SESSION) 
                 && !is_null($_SESSION['User'])
@@ -32,16 +42,19 @@
         return false;
     }
 
+    // Cette fonction sert à charger la bonne page du dossier "pages", et si la page demandée n'existe pas, il met l'accueil à la place
     function getPageTemplate($page = null){
         $fichier = TL_ROOT.'/pages/'.(is_null($page) ? 'index.php' : $page.'.php');
 
         if(!file_exists($fichier)){
-            inclde TL_ROOT.'/pages/index.php';
+            include TL_ROOT.'/pages/index.php';
         }else{
             include $fichier;
         }
     }
 
+    // Cette fonction va vérifier que l'article existe et si c'est le cas, il va aller chercher le contenu de celui-ci, le décoder et le retourner
+    // Dans le cas ou l'article n'existe pas, il renvoie "null".
     function getArticlesFromJson(){
         if(file_exist(DB_ARTICLE)) {
             $contenu_json = file_get_contents(DB_ARTICLE);
@@ -51,7 +64,11 @@
         return null;
     }
 
-    function getArticleById($id_article == null){
+    // Cette fonction va dans un premier temps vérifier que le fichier de stockage des articles existe avec "file_exists",
+    // puis va récupérer son contenu, le décoder, et parcourir chaque article pour retourner celui dont l'identifiant correspond exactement
+    // à l'$id_article fourni en argument.
+    // Si aucun article ne correspond, la fonction ne retourne rien (null).
+    function getArticleById($id_article = null){
        if(file_exists(DB_ARTICLE)) {
             $contenu_json = file_get_contents(DB_ARTICLE);
             $_articles    = json_decode($contenu_json, true);
