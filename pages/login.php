29a0652b55dd2ae88f92cb3a44b149f9f50c5f82
ParisAlan <alanparis2003@gmail.com>
2025-10-22 15:31:48 +0200

    Correction de la page login.php avec rajout de commentaire

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,16 @@
-<?php 
+<?php
+
+// Ce php va tout d'abord vérifier que la request method est bien en POST, puis par là suite, va venir vérifier que il existe bien
+// une clé login et password, et qu'elles ne sont pas vides. Après, il va faire appel à la fonction ConnectUser qui va vérifier si
+// l'identifiant et le mot de passe sont bien ceux qui sont attendus.
+
+// Dernièrement, dans le cadre ou l'utilisateur est déjà connecté ou si il a mis les bons identifiant, on le redirige directement vers l'index
+// sinon, on lui affiche un message ( variable $message ) qui indique le il vient de rentrer un mauvais login / mauvais mot de passe.
 	$message = null;
-	if($_SERVER["RQUEST_METHOD"] == "POST"){
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
 	    if(array_key_exists('login', $_POST) && array_key_exists('password', $_POST)){
 	    	if(!empty($_POST['login']) && !empty($_POST['password'])){
-	    		$_SESSION['User'] = connectUser($_GET['login'], $_POST['password']);
+	    		$_SESSION['User'] = connectUser($_POST['login'], $_POST['password']);
 
 	    		if(!is_null($_SESSION['User'])){
 	    			header("Location:index.php");
@@ -24,6 +31,7 @@
 					<a href="index.php" class="button big wide smooth-scroll-middle">Revenir à l'accueil</a></li>
 				</header>
 				<div class="content">
+<!--  Affichage dynamique du message suivant la variable $message dans le cadre ou celle-ci n'est pas null -->
 					<?php echo (!is_null($message) ? "<p>".$message."</p>" : '');?>
 					<form method="post" action="#">
 						<div class="fields">
