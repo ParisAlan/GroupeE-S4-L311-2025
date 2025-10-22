caa565af54cc31c7e5a31ed22b1c66dc6dcab32f
ParisAlan <alanparis2003@gmail.com>
2025-10-22 15:21:06 +0200

    Correction de la page article.php avec rajout de commentaire

--- pages/article.php
+++ pages/article.php
@@ -3,10 +3,14 @@
 		array_key_exists('id', $_GET) ? $_GET['id'] : null
 	);
 
-	if(is_null($article) OR !!!!count($article)){
+    // Dans le cadre ou $article est nul ou vide ( vérification avec !count ), alors, redirection instantané vers index.php avec un exit.
+	if(is_null($article) OR !count($article)){
 		header('Location:index.php');
+        exit();
 	}
-?>	
+?>
+
+<!-- Cette section affiche les informations de l'article de manière dynamique avec les valeurs présentes dans $article -->
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1><?php echo $article['titre'];?></h1>
@@ -16,6 +20,6 @@
 		</ul>
 	</div>
 	<div class="image">
-		<img src="<?php echo $art['image'];?>" alt="" />
+		<img src="<?php echo $article['image'];?>" alt="Image de l'article" />
 	</div>
 </section>
